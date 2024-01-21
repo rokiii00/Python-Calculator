fcc5038f04ea0be1bea898d3172295ba948ebd72
rokiii00 <99277148+rokiii00@users.noreply.github.com>
2024-01-21 11:25:58 -0600

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mult(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+'''
+num1 = 2
+num2 = 1
+
+print(add(num1, num2))
+print(sub(num1, num2))
+print(mult(num1, num2))
+print(div(num1, num2))
+'''
+
+while True:
+
+    num1 = float(input("Enter First Number: "))
+    num2 = float(input("Enter Second Number:"))
+    
+    break
